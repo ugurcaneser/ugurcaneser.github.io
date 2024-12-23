43d6cde255da80c88592da8e63d80deedd6e71d2
Uğurcan Eser <ugurcaneser@yahoo.com>
2024-12-23 17:48:40 +0300

    add some photos to gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -29,49 +29,49 @@ const photos = [
     },
     {
         id: 4,
-        src: '/images/gallery/photo1.jpg',
+        src: '/images/gallery/photo4.jpg',
         alt: 'Photo 4',
         width: 4096,
         height: 3072
     },
     {
         id: 5,
-        src: '/images/gallery/photo2.jpg',
+        src: '/images/gallery/photo5.jpg',
         alt: 'Photo 5',
         width: 4032,
         height: 2268
     },
     {
         id: 6,
-        src: '/images/gallery/photo3.jpg',
+        src: '/images/gallery/photo6.jpg',
         alt: 'Photo 6',
         width: 3024,
         height: 4032
     },
     {
         id: 7,
-        src: '/images/gallery/photo1.jpg',
+        src: '/images/gallery/photo7.jpg',
         alt: 'Photo 7',
         width: 4032,
         height: 3024
     },
     {
         id: 8,
-        src: '/images/gallery/photo2.jpg',
+        src: '/images/gallery/photo8.jpg',
         alt: 'Photo 8',
         width: 4032,
         height: 3024
     },
     {
         id: 9,
-        src: '/images/gallery/photo3.jpg',
+        src: '/images/gallery/photo9.jpg',
         alt: 'Photo 9',
         width: 4096,
         height: 3072
     },
     {
         id: 10,
-        src: '/images/gallery/photo3.jpg',
+        src: '/images/gallery/photo10.jpg',
         alt: 'Photo 10',
         width: 4096,
         height: 3072
